9c9539fdcd572f6e833335069a3e22139c5d0c61
Shahan Ameen <Shahanameen@gmail.com>
2023-11-27 17:00:51 -0600

    models progress

---
+++ Main/models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a course model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique:true,
+      required: true,
+      trimmed: true
+    },
+    email: {
+      type: string,
+      required:true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,"Needs to be a valid email"]
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref:"thought"
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
